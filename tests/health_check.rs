744824666110a904efb5da1cbf4d6e080dc4e73d
nscipione <nscipione@blend.com>
2025-12-29 08:40:52 -0600

    docs: inline comment

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,6 +24,9 @@ fn spawn_app() -> String {
     // Retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
     // Return the application address to the caller
     format!("http://127.0.0.1:{}", port)
